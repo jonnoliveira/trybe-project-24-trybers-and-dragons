0337ba337a72debab05aad37bdf7877718ec28c0
Jonathas Oliveira <jonnutri@gmail.com>
2023-05-08 10:05:19 -0300

    Requisito 1 - completo

---
+++ src/Races/Race.ts
@@ -0,0 +1,17 @@
+export default abstract class Race {
+  constructor(private _name: string, private _dexterity: number) {}
+
+  public static createdRacesInstances() {
+    throw new Error('Not implemented');
+  }
+
+  public get name(): string {
+    return this._name;
+  }
+
+  public get dexterity(): number {
+    return this._dexterity;
+  }
+
+  abstract get maxLifePoints(): number;
+}
